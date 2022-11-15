f3aeee427c4f6c48d5c09afef083421769c57552
CDK for Terraform Team <github-team-tf-cdk@hashicorp.com>
2022-11-15 12:45:57 +0000

    chore(release): v1.0.3

--- hcp/consulcluster/consulcluster_ConsulClusterConfig.go
+++ hcp/consulcluster/consulcluster_ConsulClusterConfig.go
@@ -52,9 +52,9 @@ type ConsulClusterConfig struct {
 	// Please be aware that the id field is automatically added to all resources in Terraform providers using a Terraform provider SDK version below 2.
 	// If you experience problems setting this value it might not be settable. Please take a look at the provider documentation to ensure it should be settable.
 	Id *string `field:"optional" json:"id" yaml:"id"`
-	// The minimum Consul version of the cluster.
+	// The minimum Consul patch version of the cluster.
 	//
-	// If not specified, it is defaulted to the version that is currently recommended by HCP.
+	// Allows only the rightmost version component to increment (E.g: `1.13.0` will allow installation of `1.13.2` and `1.13.3` etc., but not `1.14.0`). If not specified, it is defaulted to the version that is currently recommended by HCP.
 	//
 	// Docs at Terraform Registry: {@link https://www.terraform.io/docs/providers/hcp/r/consul_cluster#min_consul_version ConsulCluster#min_consul_version}
 	MinConsulVersion *string `field:"optional" json:"minConsulVersion" yaml:"minConsulVersion"`
